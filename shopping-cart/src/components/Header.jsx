51fa186a0a8ad01ed244b0af4def58d070b3c41d
ImCrimP <pjmountin@me.com>
2023-11-27 17:30:22 -0600

    created header section with some sample tabs and dropdown function

--- shopping-cart/src/components/Header.jsx
+++ shopping-cart/src/components/Header.jsx
@@ -1,4 +1,10 @@
 export default function Header() {
+  const handleItemClick = (category) => {
+    // Handle the click event for the dropdown items
+    console.log(`Clicked on ${category}`);
+    // Add your logic for handling the click event, such as navigating to a new page or updating state
+  };
+
   return (
     <div id="header">
       <img
@@ -6,6 +12,48 @@ export default function Header() {
         src="src/assets/Retail-Junction-logos-diamond.jpeg"
         alt="logo"
       />
+      <div className="tabs">
+        <div id="tab-container">
+          <a>Home</a>
+          <a>Shop</a>
+          <a>Cart</a>
+        </div>
+      </div>
+
+      <ul className="menu">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li className="dropdown">
+          <a href="#" className="dropbtn">
+            Men
+          </a>
+          <div className="dropdown-content">
+            <a href="#" onClick={() => handleItemClick("Men Clothes")}>
+              Clothes
+            </a>
+            <a href="#" onClick={() => handleItemClick("Men Accessories")}>
+              Accessories
+            </a>
+            {/* Add more categories as needed */}
+          </div>
+        </li>
+        <li className="dropdown">
+          <a href="#" className="dropbtn">
+            Women
+          </a>
+          <div className="dropdown-content">
+            <a href="#" onClick={() => handleItemClick("Women Clothes")}>
+              Clothes
+            </a>
+            <a href="#" onClick={() => handleItemClick("Women Accessories")}>
+              Accessories
+            </a>
+            {/* Add more categories as needed */}
+          </div>
+        </li>
+        {/* Add more top-level menu items as needed */}
+      </ul>
     </div>
   );
 }
